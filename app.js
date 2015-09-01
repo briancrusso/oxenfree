44216d61415ebc5fa7569455d2edd6018ae5e973
“briancrussogit config--global user.email russodraw@gmail.com <russodraw@gmail.com>
2015-09-01 10:30:24 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$('.box1').click (function(){
+		$('.boxes').hide();
+		$('.about').show();
+		$('.about').css('display','table');
+	})
+	$('.home').click (function(){
+		$('.about').hide();
+		$('.boxes').show();
+	});
+});
