f0216bbceb56e00b96c5eb6492a7121570275248
“briancrussogit config--global user.email russodraw@gmail.com <russodraw@gmail.com>
2015-09-09 10:46:27 -0400

    switching cell phone image

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
 $(document).ready(function(){
-	$('.box1').click (function(){
-		$('.boxes').hide();
-		$('.about').show();
-		$('.about').css('display','table');
-	})
 	$('.home').click (function(){
 		$('.about').hide();
 		$('.boxes').show();
