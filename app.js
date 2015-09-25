8d0fd2d09d26c8f54844df6d2ce8b088aabaf20b
“briancrussogit config--global user.email russodraw@gmail.com <russodraw@gmail.com>
2015-09-25 12:32:23 -0400

    javascript for iphones

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         $('.menu').toggle();
     })
     if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
-        $(".change-this-class li a").click(function() {
+        $(".block").click(function() {
             // Update '.change-this-class' to the class of your menu
             // Leave this empty, that's the magic sauce
         });
