55b63b063eb22492d4926243a2cf2f26675d8aa6
“briancrussogit config--global user.email russodraw@gmail.com <russodraw@gmail.com>
2015-09-25 12:26:57 -0400

    javascript for rollovers on iphone

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-$(document).ready(function(){
-	$('.home').click (function(){
-		$('.about').hide();
-		$('.boxes').show();
-	});
-		$('.fa-bars').click (function(){
-		$('.menu').toggle();
-	})
+$(document).ready(function() {
+    $('.fa-bars').click(function() {
+        $('.menu').toggle();
+    })
+    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
+        $(".change-this-class li a").click(function() {
+            // Update '.change-this-class' to the class of your menu
+            // Leave this empty, that's the magic sauce
+        });
+    }
 });
