743a9afa3c36dd1015c12662809447c3d3ee1a37
“briancrussogit config--global user.email russodraw@gmail.com <russodraw@gmail.com>
2015-09-28 09:37:11 -0400

    updating images for phones

--- app.js
+++ app.js
@@ -2,10 +2,4 @@ $(document).ready(function() {
     $('.fa-bars').click(function() {
         $('.menu').toggle();
     })
-    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i))) {
-        $(".block").click(function() {
-            // Update '.change-this-class' to the class of your menu
-            // Leave this empty, that's the magic sauce
-        });
-    }
 });
